2ef39077bc926acfa6e6d66e952648daefee2c63
Srujan M <srujanvarma30@gmail.com>
2017-08-01 16:01:57 -0500

    Added Hello world project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            c1.i = 10;
+            c1.
+            // Print hellow World..!!
+            Console.WriteLine("Hello World..!!");
+
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+
+            Console.ReadLine();
+        }
+    }
+}
